a6389cccb74e8c9b38ac7d8c16cd7578216e6f34
DimensionSTP <ddang8jh@gmail.com>
2025-09-01 03:48:51 +0900

    refactor: default script name, args

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-data_type="conversational"
-dataset_name="mmlu"
-is_preprocessed=False
+dataset_name="documents"
 upload_user="Qwen"
-model_type="Qwen3-235B-A22B"
+model_type="Qwen3-30B-A3B-Instruct-2507"
 revision="main"
 left_padding=True
-max_length=2048
-max_new_tokens=256
+max_new_tokens=1024
 do_sample=True
 temperature=0.6
 top_p=0.95
 top_k=20
 
-python main.py mode=test_vllm \
-    data_type=$data_type \
+python main.py mode=generate \
     dataset_name=$dataset_name \
-    is_preprocessed=$is_preprocessed \
     upload_user=$upload_user \
     model_type=$model_type \
     revision=$revision \
     left_padding=$left_padding \
-    max_length=$max_length \
     max_new_tokens=$max_new_tokens \
     do_sample=$do_sample \
     generation_config.temperature=$temperature \
